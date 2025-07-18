cbd628349f69da19ed7ad56d5cbe2391f0eaa84f
focalored <175320414+focalored@users.noreply.github.com>
2025-07-18 10:36:06 -0500

    refactor: Split round builder

---
+++ models/Round.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const roundSchema = new mongoose.Schema({
+  sessionId: { type: mongoose.Schema.Types.ObjectId, ref: "VotingSession" },
+  roundNumber: Number,
+  evalMode: String,
+  candidates: [String],
+  votes: [{
+    candidateId: String,
+    count: Number
+  }],
+  result: Object
+}, { timestamps: true });
+
+roundSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.__v;
+  }
+});
+
+
+module.exports = mongoose.model("Round", roundSchema);
